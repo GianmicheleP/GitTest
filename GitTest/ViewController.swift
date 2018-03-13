592798b7c19684e044d83d847f9fd07b6db19dc5
Gianmichele Persico <gianmichelepersico@cam-12.local>
2018-03-13 11:52:57 +0100

    Creato layout e contatore

--- GitTest/ViewController.swift
+++ GitTest/ViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mess: UILabel!
+    
+    var contatore = 0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +23,11 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func puls(_ sender: Any) {
+        contatore = contatore + 1
+        mess.text = "hai premuto \(contatore) volte"
+    }
+    
 
 }
 
